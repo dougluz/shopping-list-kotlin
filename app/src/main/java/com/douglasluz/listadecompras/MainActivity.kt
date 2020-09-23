816e76054b01d2b7ef724d7c5398fe4cd69e05e7
Douglas de Souza Luz <dougs.2210@gmail.com>
2020-09-22 22:29:59 -0300

    criado metodo para remover items da lista

--- app/src/main/java/com/douglasluz/listadecompras/MainActivity.kt
+++ app/src/main/java/com/douglasluz/listadecompras/MainActivity.kt
@@ -2,6 +2,8 @@ package com.douglasluz.listadecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,6 +25,12 @@ class MainActivity : AppCompatActivity() {
             } else {
                 text_product.error = "Please type something"
             }
+        }
+
+        items_list.setOnItemLongClickListener { _, view: View?, position: Int, id: Long ->
+            val item = productsAdapter.getItem(position)
+            productsAdapter.remove(item)
+            true
         }
     }
 }
